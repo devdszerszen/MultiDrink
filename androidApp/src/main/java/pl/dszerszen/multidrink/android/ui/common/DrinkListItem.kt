82fafa487501969b019fdc8eeaefb3e66bc7b3ba
Damian Szerszen <dev.dszerszen@gmail.com>
2023-01-19 20:15:37 +0100

    minor changes in android ui

--- androidApp/src/main/java/pl/dszerszen/multidrink/android/ui/common/DrinkListItem.kt
+++ androidApp/src/main/java/pl/dszerszen/multidrink/android/ui/common/DrinkListItem.kt
@@ -1,7 +1,6 @@
 package pl.dszerszen.multidrink.android.ui.common
 
 import android.content.res.Configuration
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -9,7 +8,6 @@ import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import pl.dszerszen.multidrink.android.MyApplicationTheme
@@ -21,8 +19,7 @@ fun DrinkListItem(
     modifier: Modifier = Modifier
 ) {
     Card(
-        modifier = modifier,
-        border = BorderStroke(1.dp, Color.Red)
+        modifier = modifier
     ) {
         Row(modifier = Modifier.fillMaxWidth()) {
             Text(
--- androidApp/src/main/java/pl/dszerszen/multidrink/android/ui/search/SearchScreen.kt
+++ androidApp/src/main/java/pl/dszerszen/multidrink/android/ui/search/SearchScreen.kt
@@ -4,17 +4,16 @@ import android.content.res.Configuration
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Face
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -46,59 +45,67 @@ private fun SearchScreen(
     Scaffold(
         modifier = modifier
     ) { paddingValues ->
-        LazyColumn(
-            modifier = Modifier
-                .padding(paddingValues)
-                .fillMaxSize(),
-            contentPadding = PaddingValues(8.dp),
-            verticalArrangement = Arrangement.spacedBy(4.dp)
-        ) {
-            item {
-                Column(modifier = Modifier.fillMaxWidth()) {
-                    OutlinedTextField(
-                        modifier = Modifier.fillMaxWidth(),
-                        value = state.searchInput,
-                        onValueChange = { onIntent(SearchUiIntent.OnInputChanged(it)) },
-                        label = { Text("Input drink name") },
-                        trailingIcon = {
-                            if (state.isLoading) {
-                                CircularProgressIndicator(
-                                    modifier = Modifier.size(16.dp)
-                                )
-                            }
-                        },
-                        isError = state.errorMessage.isNullOrEmpty().not()
-                    )
-                    AnimatedVisibility(
-                        visible = state.errorMessage.isNullOrEmpty().not(),
-                        enter = expandVertically(),
-                        exit = shrinkVertically()
-                    ) {
-                        Text(
-                            text = state.errorMessage.orEmpty(),
-                            color = Color.Red
+        Column(Modifier.fillMaxSize()) {
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                value = state.searchInput,
+                onValueChange = { onIntent(SearchUiIntent.OnInputChanged(it)) },
+                label = { Text("Input drink name") },
+                trailingIcon = {
+                    if (state.isLoading) {
+                        CircularProgressIndicator(
+                            modifier = Modifier.size(16.dp)
                         )
                     }
-                }
-            }
-            if (state.isInitialState) {
+                },
+                isError = state.errorMessage.isNullOrEmpty().not()
+            )
+            LazyColumn(
+                modifier = Modifier
+                    .background(Color(233, 233, 233, 255))
+                    .padding(paddingValues)
+                    .fillMaxSize(),
+                contentPadding = PaddingValues(8.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
                 item {
-                    Column(
-                        Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp)) {
-                        Icon(Icons.Default.Face, null,
-                            Modifier
-                                .size(128.dp)
-                                .align(CenterHorizontally))
-                        Spacer(Modifier.height(8.dp))
-                        Text("Delicious drinks are waiting for you, check it out!")
+                    Column(modifier = Modifier.fillMaxWidth()) {
+                        AnimatedVisibility(
+                            visible = state.errorMessage.isNullOrEmpty().not(),
+                            enter = expandVertically(),
+                            exit = shrinkVertically()
+                        ) {
+                            Text(
+                                text = state.errorMessage.orEmpty(),
+                                color = Color.Red
+                            )
+                        }
                     }
                 }
-            }
-            if (state.drinks.isNotEmpty()) {
-                items(state.drinks, { drink -> drink.id }) {
-                    DrinkListItem(it)
+                if (state.isInitialState) {
+                    item {
+                        Column(
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        ) {
+                            Icon(
+                                Icons.Default.Face, null,
+                                Modifier
+                                    .size(32.dp)
+                                    .align(CenterHorizontally)
+                            )
+                            Spacer(Modifier.height(8.dp))
+                            Text("Delicious drinks are waiting for you, check it out!")
+                        }
+                    }
+                }
+                if (state.drinks.isNotEmpty()) {
+                    items(state.drinks, { drink -> drink.id }) {
+                        DrinkListItem(it)
+                    }
                 }
             }
         }
